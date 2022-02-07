c9cbf20e81a3b7cccf9b81a418080e5c9a796ac7
carlslinasr <carl.salinasr@duocuc.cl>
2022-02-07 15:32:06 -0300

    repaso

---
+++ js-repaso/index.js
@@ -0,0 +1,16 @@
+
+// formas de declarar variables
+
+
+console.log(miVariable)
+
+
+var miVariable = 'variable'
+miVariable = 'otro valor'
+
+let variable = 'variable creada con let'
+variable = 'otro valor a let'
+
+const constante = 'este valor es constante'
+
+console.log(miVariable,variable)
